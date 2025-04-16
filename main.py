acff64887f2bb803fee3e5ff6b1c2337deaf3066
Foufou.exe <thibaut.maurras34@gmail.com>
2025-04-16 17:55:45 +0200

    Ajout de la fonction load_streams pour charger les flux depuis un fichier JSON et mise à jour de la logique principale pour sélectionner un flux aléatoire.

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-# main.py
+# -*- coding: utf-8 -*-
+# Libraries
+import random
+import json
+# Modules
 from modules.stream import StreamCapture
 from modules.hash import EntropyGenerator
 
-def main():
-    youtube_url = "https://www.youtube.com/watch?v=yuhnCtTPtZo"
-    
-    print("🌊 Connexion au flux vidéo...")
-    stream = StreamCapture(youtube_url)
-    
-    print("📦 Capture d'une frame...")
-    frame = stream.capture_frame()
-    
-    print("🔥 Génération de l'entropie...")
-    entropy = EntropyGenerator.hash_frame(frame)
-    
-    print("✨ Clé aléatoire générée depuis l'aquarium :", entropy)
+def load_streams(json_path="./data/streams_url.json") -> list:
+    with open(json_path, "r") as file:
+        data = json.load(file)
+        return data.get("youtube_streams", [])
 
+def main():
+    streams = load_streams()
+    if not streams:
+        raise Exception("💀 Aucun flux trouvé dans le fichier JSON.")
+
+    selected_stream = random.choice(streams)
+    name = selected_stream["name"]
+    url = selected_stream["url"]
+    
+    print(f"🎥 Flux sélectionné : {name}")
+    print(f"🔗 URL : {url}")
+
+    print("🔄 Connexion au flux vidéo...")
+    stream = StreamCapture(url)
+
+    print("📸 Capture d'une frame...")
+    frame = stream.capture_frame()
+
+    print("🔐 Génération de l'entropie...")
+    entropy = EntropyGenerator.hash_frame(frame)
+
+    print(f"✨ Clé aléatoire générée depuis « {name} » :", entropy)
 if __name__ == "__main__":
     main()
